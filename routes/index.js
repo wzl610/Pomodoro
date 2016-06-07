7f2e539a65f300526b5783f44b46983488902855
Allen <237984496@qq.com>
2016-06-07 10:47:36 +0800

    modify routes

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,10 @@ module.exports = function(app){
 	app.get('/', function(req, res) {
 	  res.render('index', { title: 'Test' });
 	});
+
+	app.post('/login',function(req,res){
+		console.log(req.body.username);
+		console.log(req.body.password);
+	});
 };
 
